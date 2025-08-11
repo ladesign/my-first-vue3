eb5b6ec4c2ccf4ba8a11e7bc84cb84b8f944ef27
ladesign <jeywang@gmail.com>
2025-08-11 13:32:50 +0800

    添加 Vite 專案的 base

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/my-first-vue3/', // ⭐ 重點：這裡填 GitHub 遠端儲存庫名稱
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
